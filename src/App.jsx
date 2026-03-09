a97189d1a9cf3efe75bd100855baeb28de632f72
DepressedPotatok <mj.mjahan045@gmail.com>
2026-03-09 15:19:30 +0600

    card layout fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/Navbar'
 import Banner from './components/Banner/Banner'
 import Players from './components/Players/Players'
 import { PlayersContext } from './components/PlayersContext'
+import Loader from './components/ui/Loader'
 
 const fetchPlayers = async () => {
   const res = await fetch("/players.json")
@@ -13,15 +14,21 @@ const fetchPlayers = async () => {
 function App() {
   const playersPromise = useMemo(() => fetchPlayers(), [])
 
+  const [freeCoins, setFreeCoins] = useState(false)
   const [balance, setBalance] = useState(6000000)
   const [selectPlayer, setSelectPlayer] = useState([])
+
+  function handleFreeCoins() {
+    setFreeCoins(prev => !prev)
+    setBalance(prev => prev + 10000)
+  }
 
   function handleChoosePlayer(selectedData) {
     setSelectPlayer(prev => [...prev, selectedData])
     setBalance(prev => prev - selectedData.price)
   }
 
-  function handleDeletePlayer (deletedData) {
+  function handleDeletePlayer(deletedData) {
     const filteredData = selectPlayer.filter(player => player.playerName !== deletedData.playerName)
     setSelectPlayer(filteredData)
     setBalance(prev => prev + deletedData.price)
@@ -30,12 +37,15 @@ function App() {
   return (
     <>
       <Navbar balance={balance} />
-      <Banner />
+      <Banner freeCoins={freeCoins} handleFreeCoins={handleFreeCoins} />
 
       <PlayersContext.Provider value={{ playersPromise, handleChoosePlayer, selectPlayer, handleDeletePlayer }}>
-        <Suspense fallback={<span className="loading loading-dots loading-lg"></span>}>
-          <Players />
-        </Suspense>
+        <main>
+          <Suspense fallback={<Loader />}>
+            <Players />
+          </Suspense>
+        </main>
+
       </PlayersContext.Provider>
     </>
   )
